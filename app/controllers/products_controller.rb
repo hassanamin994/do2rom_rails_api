36ce6c85142e73538416290439ecb9555ed6662e
dlinah <eng.lina.a92@gmail.com>
2017-05-31 11:27:15 +0200

    setting production options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,7 +15,7 @@ class ProductsController < ApplicationController
 
   # POST /products
   def create
-    @product = Product.new(product_params.as_json)
+    @product = Product.new(product_params)
 
     if @product.save
       render json: @product, status: :created, location: @product
