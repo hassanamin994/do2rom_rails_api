c84b743e55a4e23621e36c808ecd463cc3ff6ff5
dlinah <eng.lina.a92@gmail.com>
2017-06-03 21:53:50 +0200

    commenting image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:email ,:password,:password_confirmation)
+      params.require(:user).permit(:email ,:password,:password_confirmation, :username)
     end
 end
