f79d1bbb0c54f43ec9e9c69b36bfb222cd9cbf20
dlinah <eng.lina.a92@gmail.com>
2017-06-06 13:03:47 +0200

    resolving conflict

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,5 @@
 class User
   include Mongoid::Document
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  ## Database authenticatable
-  field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
-  field :avtar, type: String
-
-  ## Recoverable
-  field :reset_password_token,   type: String
-  field :reset_password_sent_at, type: Time
-
-  ## Rememberable
-  field :remember_created_at, type: Time
-
-  ## Trackable
-  field :sign_in_count,      type: Integer, default: 0
-  field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
-  field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
-
-  ## Confirmable
-  # field :confirmation_token,   type: String
-  # field :confirmed_at,         type: Time
-  # field :confirmation_sent_at, type: Time
-  # field :unconfirmed_email,    type: String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
-  # field :locked_at,       type: Time
   include ActiveModel::SecurePassword
   include ActiveModel::Validations
   field :email, type: String
@@ -47,5 +13,4 @@ class User
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: [ :create, :update ] }
   validates :username, presence: true
   has_secure_password  #authenticate method
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
+
 Rails.application.routes.draw do
-  devise_for :users
+  post 'user_token' => 'user_token#create'
   root to: 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
   	resources :prices, only: [:index, :create]
   end
   resources :prices, only: [:show, :edit, :update, :destroy]
+  post 'prices/:id/confirm' => 'prices#confirm'
+  post 'prices/:id/disconfirm' => 'prices#disconfirm'
+  get '/products/search/:seraching_word', to: 'products#search'
+  resources :users  #knock added routes
 end
