5714db29303b7821494a06fc78c5b885c438e63b
dlinah <eng.lina.a92@gmail.com>
2017-06-06 15:10:27 +0200

    comment crud

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :products do
   	resources :prices, only: [:index, :create]
+  	resources :comments, only: [:index, :create]
   end
+  resources :prices, only: [:show, :edit, :update, :destroy]
+  resources :comments, only: [:show, :edit, :update, :destroy]
   get '/products/search/:seraching_word', to: 'products#search'
   get '/products/searchqr/:seraching_qr', to: 'products#searchqr'
-  resources :prices, only: [:show, :edit, :update, :destroy]
   post 'prices/:id/confirm' => 'prices#confirm'
   post 'prices/:id/disconfirm' => 'prices#disconfirm'
   resources :users  #knock added routes
