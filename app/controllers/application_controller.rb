24147f9083e73326a84bf5200ab86bbf9ea16fe7
dlinah <eng.lina.a92@gmail.com>
2017-06-08 13:46:01 +0200

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
 	require 'carrierwave'
 	include Knock::Authenticable
-	before_action :authenticate_user :except => ["api"]
-	
+		unless controller_name == 'api'
+			before_action :authenticate_user
+		end 
 end
