3a9448bd3a10e1b45fe1a8d490ec2b1e355f2c4c
dlinah <eng.lina.a92@gmail.com>
2017-06-09 16:39:28 +0200

    comment product

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
   belongs_to :user, dependent: :delete, validate: false
+  belongs_to :product, dependent: :delete, validate: false
   field :body , type: String
 
 end
