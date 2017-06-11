f6eaf1adaa61c57eabfdd053a8960ee2d0afae31
dlinah <eng.lina.a92@gmail.com>
2017-06-11 11:55:10 +0200

    l

--- app/serializers/products/show_serializer.rb
+++ app/serializers/products/show_serializer.rb
@@ -1,15 +1,9 @@
 class Products::ShowSerializer < ProductSerializer
-	attributes :id, :name, :qr_code
+	attributes :id, :name, :qr_code, :details
 	class CustomPriceSerializer < PriceSerializer
-		attributes :id, :location, :sale, :sale_percentage, :sale_expiration, :price, :image, :confirmations, :disconfirmations, :user
+		attributes :id, :location, :sale, :sale_percentage, :sale_expiration, :price, :image, :confirmation_ids, :disconfirmation_ids, :user
   		def user
   			{id: object.user.id, username: object.user.username}
-  		end
-  		def confirmations
-  			user
-  		end
-  		def disconfirmations
-  			user
   		end
   	end
 
@@ -20,7 +14,21 @@ class Products::ShowSerializer < ProductSerializer
   		end
   	end
 
-  	has_many :comments, serializer: CustomCommentSerializer
+  def details
+    if object.prices.exists?
+      prices = object.prices.order_by(:price => 'desc')
+      { max: prices.first.price, max_id: prices.first.id, min: prices.last.price, min_pic: prices.last.image, min_id: prices.last.id, 
+        min_username: prices.last.user.username, min_userid: prices.last.user.id, 
+        min_location: prices.last.location, min_sale: prices.last.sale, 
+        min_sale_percentage: prices.last.sale_percentage, min_sale_expiration: prices.last.sale_expiration, 
+        min_confirmations:prices.last.confirmation_ids, 
+        min_disconfirmations:prices.last.disconfirmation_ids}
+    else
+      nil
+    end
+  end
+  
+  has_many :comments, serializer: CustomCommentSerializer
 
  	has_many :prices, serializer: CustomPriceSerializer
 
