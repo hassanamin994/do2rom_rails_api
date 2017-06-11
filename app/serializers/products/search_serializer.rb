468d77ddb47db12d8c75c25ca68dc4a878b1ac6e
dlinah <eng.lina.a92@gmail.com>
2017-06-11 02:03:17 +0200

    l

--- app/serializers/products/search_serializer.rb
+++ app/serializers/products/search_serializer.rb
@@ -3,7 +3,7 @@ class Products::SearchSerializer < ProductSerializer
 	def details
 		if object.prices.exists?
 			prices = object.prices.order_by(:price => 'desc')
-			{ max: prices.first.price, min: prices.last.price, min_pic: prices.last.image, min_uername: prices.last.user.username, min_userid: prices.last.user.id}
+			{ max: prices.first.price, min: prices.last.price, min_pic: prices.last.image, min_uername: prices.last.user.username, min_userid: prices.last.user.id, min_confirmations:prices.last.confirmation_ids, min_disconfirmations:prices.last.disconfirmation_ids}
 		else
 			nil
 		end
