0096382edf2c53dd8ebdf62e7e0d451cea0ea129
dlinah <eng.lina.a92@gmail.com>
2017-06-11 12:22:47 +0200

    l

--- app/serializers/products/show_serializer.rb
+++ app/serializers/products/show_serializer.rb
@@ -17,8 +17,8 @@ class Products::ShowSerializer < ProductSerializer
   def details
     if object.prices.exists?
       prices = object.prices.order_by(:price => 'desc')
-      { max: prices.first.price, max_id: prices.first.id, min: prices.last.price, min_pic: prices.last.image, min_id: prices.last.id, 
-        min_username: prices.last.user.username, min_userid: prices.last.user.id, 
+      { max: prices.first.price, max_id: prices.first.id.to_s, min: prices.last.price, min_pic: prices.last.image, min_id: prices.last.id, 
+        min_username: prices.last.user.username, min_userid: prices.last.user.id.to_s, 
         min_location: prices.last.location, min_sale: prices.last.sale, 
         min_sale_percentage: prices.last.sale_percentage, min_sale_expiration: prices.last.sale_expiration, 
         min_confirmations:prices.last.confirmation_ids, 
