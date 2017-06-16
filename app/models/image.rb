e803fdcac79eab93a25b950c47256bf552209397
dlinah <eng.lina.a92@gmail.com>
2017-06-16 17:26:29 +0200

    remove ss

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,5 @@
 class Image
   include Mongoid::Document
-  include Rails.application.routes.url_helpers
   mount_base64_uploader :image, AvatarUploader, file_name: -> (i) { i.id }
-  field :ss, type: String
   embedded_in :Product
 end
