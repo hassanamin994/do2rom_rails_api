ec45e8e27dd0944c69d345259e501ac6b3facc30
hassanamin994 <hassanmamin994@gmail.com>
2018-01-31 17:38:54 +0200

    Fix cors

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,7 +6,7 @@ class ApiController < ApplicationController
   def cors_set_access_control_headers
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, X-Requested-With, X-Prototype-Version, Token, Authorization, Content-Type'
     headers['Access-Control-Max-Age'] = "1728000"
   end
  
@@ -14,7 +14,7 @@ class ApiController < ApplicationController
     if request.method == 'OPTIONS'
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token, Authorization, Content-Type'
+      headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, X-Requested-With, X-Prototype-Version, Token, Authorization, Content-Type'
       headers['Access-Control-Max-Age'] = '1728000'
  
       render text: 'true', content_type: 'text/plain' ,status: :ok
